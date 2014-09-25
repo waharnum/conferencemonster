6e79a0ed52f33a666ab6fc38f3a9435ad0c14f26
Alan Harnum <waharnum@gmail.com>
2014-09-25 16:26:59 -0500

    Begin xml generation

--- scrapers/il2014.rb
+++ scrapers/il2014.rb
@@ -2,6 +2,9 @@ require 'nokogiri'
 require 'open-uri'
 
 class Speaker
+  attr_accessor :name
+  attr_accessor :job_title
+  attr_accessor :works_for
   def initialize(name, job_title,works_for)
     @name = name
     @job_title = job_title    
@@ -10,6 +13,9 @@ class Speaker
 end
 
 class Session
+  attr_accessor :title
+  attr_accessor :description
+  attr_accessor :speakers
   def initialize(title, description)
     @title = title
     @description = description
@@ -27,6 +33,9 @@ class Session
 end
 
 class Conference
+  attr_accessor :title
+  attr_accessor :year
+  attr_accessor :sessions
   def initialize(title, year)
     @title = title
     @year = year
@@ -39,9 +48,9 @@ end
 
 conference = Conference.new("Internet Librarian","2014")
 
-# addies = ['il2014-monday.html','il2014-tuesday.html','il2014-wednesday.html','il2014-workshops.html']
+addies = ['il2014-monday.html','il2014-tuesday.html','il2014-wednesday.html','il2014-workshops.html']
 
-addies = ['il2014-workshops.html']
+# addies = ['il2014-workshops.html']
 
 addies.each do |addy|
 	doc = Nokogiri::HTML(open(addy))
@@ -51,26 +60,35 @@ addies.each do |addy|
 
   	session_title = session.xpath('h3[@class="session-title"]').inner_html
   	session_description = session.xpath('span[@itemprop="description"]').inner_html
-
-    # puts "Index/Inspect: #{session_index} - #{session.inspect}"
-    # puts "Title/Description: #{session_title} - #{session.description}"
-
-
+    
     session_obj = Session.new(session_title, session_description)
 
-  	# puts "#{session_title}"
-    # puts "#{session_description}"
     session.xpath('div[@class="program-speaker"]').each do |session_speaker|
       speaker_name = session_speaker.xpath('a/span[@itemprop="name"]').inner_html
       speaker_job_title = session_speaker.xpath('span[@itemprop="jobTitle"]').inner_html
       speaker_works_for = session_speaker.xpath('span[@itemprop="worksFor"]/span[contains(@itemprop, "name")]').inner_html      
-      # puts "\t#{speaker_name}, #{speaker_job_title}, #{speaker_works_for}"        
       speaker = Speaker.new(speaker_name,speaker_job_title,speaker_works_for)
       session_obj.add_speaker(speaker)
     end
     
-    puts session_obj
+    # puts session_obj
     conference.add_session(session_obj)
 
 	end
 end
+
+builder = Nokogiri::XML::Builder.new do |xml|
+  xml.conference{
+    xml.title conference.title
+    xml.year conference.year
+    xml.sessions {
+      conference.sessions.each do |s|
+        xml.session {
+          xml.title s.title
+          xml.description s.description
+        }
+      end
+    }
+  }
+end
+puts builder.to_xml
