7d7bd41ae2a41481417f234c901cc6bc8bc87b86
Alan Harnum <waharnum@gmail.com>
2014-09-25 15:39:47 -0500

    Not sure why doublecounting events :( saving work for now to investigate later

--- scrapers/il2014.rb
+++ scrapers/il2014.rb
@@ -1,17 +1,75 @@
-
 require 'nokogiri'
 require 'open-uri'
 
-addies = ['il2014-monday.html','il2014-tuesday.html','il2014-wednesday.html','il2014-workshops.html']
+class Speaker
+  def initialize(name, job_title,works_for)
+    @name = name
+    @job_title = job_title    
+    @works_for = works_for
+  end
+end
+
+class Session
+  def initialize(title, description)
+    @title = title
+    @description = description
+    @speakers = Array.new()
+  end
+
+  def add_speaker(speaker)
+    @speakers.push(speaker)
+  end
+
+  def to_s()
+    return "#{@title}, #{@description}, #{@speakers}"
+  end
+
+end
+
+class Conference
+  def initialize(title, year)
+    @title = title
+    @year = year
+    @sessions = Array.new()
+  end
+  def add_session(session)
+    @sessions.push(session)
+  end
+end
+
+conference = Conference.new("Internet Librarian","2014")
+
+# addies = ['il2014-monday.html','il2014-tuesday.html','il2014-wednesday.html','il2014-workshops.html']
+
+addies = ['il2014-workshops.html']
 
 addies.each do |addy|
 	doc = Nokogiri::HTML(open(addy))
-  doc.xpath('//div[contains(@class,"session")]').each do |session|
+  
+  doc.xpath('//div[contains(@class,"session")]').each_with_index do |session, session_index|    
+
+  	session_title = session.xpath('h3[contains(@class,"session-title")]').inner_html
+  	session_description = session.xpath('span[contains(@itemprop,"description")]').inner_html
+
+    # puts "Index/Inspect: #{session_index} - #{session.inspect}"
+    # puts "Title/Description: #{session_title} - #{session.description}"
+
+
+    session_obj = Session.new(session_title, session_description)
+
+  	# puts "#{session_title}"
+    # puts "#{session_description}"
     session.xpath('div[contains(@class,"program-speaker")]').each do |session_speaker|
-      # puts "SPEAKER: #{session_speaker}"
-      session_speaker.xpath('a/span[contains(@itemprop, "name")]').each do |speaker_name|
-        puts "\t#{speaker_name.content}"
-      end
+      speaker_name = session_speaker.xpath('a/span[contains(@itemprop, "name")]').inner_html
+      speaker_job_title = session_speaker.xpath('span[contains(@itemprop, "jobTitle")]').inner_html
+      speaker_works_for = session_speaker.xpath('span[contains(@itemprop, "worksFor")]/span[contains(@itemprop, "name")]').inner_html      
+      # puts "\t#{speaker_name}, #{speaker_job_title}, #{speaker_works_for}"        
+      speaker = Speaker.new(speaker_name,speaker_job_title,speaker_works_for)
+      session_obj.add_speaker(speaker)
     end
+    
+    puts session_obj
+    conference.add_session(session_obj)
+
 	end
 end
