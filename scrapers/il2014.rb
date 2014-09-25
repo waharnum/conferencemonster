b2a32d6c8c71ce2d9e237f21584eaa1fc899fa3e
Alan Harnum <waharnum@gmail.com>
2014-09-25 18:53:23 +0000

    xpath noodling

--- scrapers/il2014.rb
+++ scrapers/il2014.rb
@@ -2,11 +2,16 @@
 require 'nokogiri'
 require 'open-uri'
 
-addies = ['http://internet-librarian.infotoday.com/2014/Workshops.aspx','http://internet-librarian.infotoday.com/2014/Monday.aspx','http://internet-librarian.infotoday.com/2014/Tuesday.aspx','http://internet-librarian.infotoday.com/2014/Wednesday.aspx']
+addies = ['il2014-monday.html','il2014-tuesday.html','il2014-wednesday.html','il2014-workshops.html']
 
 addies.each do |addy|
 	doc = Nokogiri::HTML(open(addy))
-	doc.xpath('//div[contains(@class,"session")]').each do |session|
-		puts session		
+  doc.xpath('//div[contains(@class,"session")]').each do |session|
+    session.xpath('div[contains(@class,"program-speaker")]').each do |session_speaker|
+      # puts "SPEAKER: #{session_speaker}"
+      session_speaker.xpath('a/span[contains(@itemprop, "name")]').each do |speaker_name|
+        puts "\t#{speaker_name.content}"
+      end
+    end
 	end
 end
