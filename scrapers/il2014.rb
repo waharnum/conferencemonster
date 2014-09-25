c83e11ffbf6d00bb8e536ebaba7122429e578797
Alan Harnum <waharnum@gmail.com>
2014-09-25 12:19:04 -0500

    initial scraper tomfoolery & haberdashery

---
+++ scrapers/il2014.rb
@@ -0,0 +1,12 @@
+
+require 'nokogiri'
+require 'open-uri'
+
+addies = ['http://internet-librarian.infotoday.com/2014/Workshops.aspx','http://internet-librarian.infotoday.com/2014/Monday.aspx','http://internet-librarian.infotoday.com/2014/Tuesday.aspx','http://internet-librarian.infotoday.com/2014/Wednesday.aspx']
+
+addies.each do |addy|
+	doc = Nokogiri::HTML(open(addy))
+	doc.xpath('//h3[contains(@class,"session-title")]').each do |link|
+		puts link
+	end
+end
