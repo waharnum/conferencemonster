39d77c3f3fcdc71254bfbf293c8d9447c21c7bb1
Alan Harnum <waharnum@gmail.com>
2015-02-13 11:20:26 -0500

    more basics

--- app/controllers/conferences_controller.rb
+++ app/controllers/conferences_controller.rb
@@ -1,6 +1,18 @@
 class ConferencesController < ApplicationController
 
+	before_action :set_conference, only: [:show]
+
 	def index
     	@conferences = Conference.all
   	end
+
+  	def show
+  	end
+
+	private
+	    # Use callbacks to share common setup or constraints between actions.
+	    def set_conference
+	      @conference = Conference.find(params[:id])
+	    end	
+
 end
